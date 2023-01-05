704f24a3ff4fac50ee6f6b3fc919845f7a04bda4
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-01-05 13:24:44 -0800

    basically styling for homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import "./App.css";
+import Home from "./components/home";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <h1 className="home-title">Quizzical</h1>
+      <h2 className="home-description">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat ipsam
+        soluta alias aut rerum tenetur illo corporis laudantium, nisi similique!
+        Officiis quis doloribus rem sequi molestias architecto quas aliquam ad.
+      </h2>
+      <button className="home-start-button">Start Quiz</button>
+    </div>
+  );
+}
