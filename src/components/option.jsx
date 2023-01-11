55e7793a1a78abc45026f3d0feecdc66016b035b
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-01-10 22:03:56 -0800

    adding state for selectedAnswer

--- src/components/option.jsx
+++ src/components/option.jsx
@@ -1,18 +1,12 @@
 import { useState } from "react";
 import { decode } from "html-entities";
 
-export default function Option(props) {
-  const [isSelected, setIsSelected] = useState(false);
-  const styles = { backgroundColor: isSelected ? "#D6DBF5" : "transparent" };
+export default function Option({ op, handleSelection, handleSelectedAnswer }) {
+  const styles = { backgroundColor: false ? "#D6DBF5" : "transparent" };
 
-  function handleSelect() {
-    setIsSelected((previous) => !previous);
-    //update Selected Answer to this components option
-    props.handleSelectedAnswer(props.op);
-  }
   return (
-    <div className="quiz-button" style={styles} onClick={handleSelect}>
-      {decode(props.op)}
+    <div className="quiz-button" style={styles} onClick={handleSelection}>
+      {decode(op)}
     </div>
   );
 }
