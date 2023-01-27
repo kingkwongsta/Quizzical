af862927e8d707ced2f3eb01d625d2355e310dfa
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-01-26 21:41:15 -0800

    cleanup comments and formatting

--- src/data.js
+++ src/data.js
@@ -1,15 +1,5 @@
 import React from "react";
 
-// export const fetchData = async () => {
-//   console.log("fetching data");
-//   const response = await fetch(
-//     "https://opentdb.com/api.php?amount=5&type=multiple"
-//   );
-//   const data = await response.json();
-//   const data2 = await data.results.map((x) => x);
-//   return data.results;
-// };
-
 export const fetchData = async () => {
   console.log("fetching data");
   const response = await fetch(
